3117558f0957119c3670fff2757d30fea740d18c
Gökalp Avcu <avcu219@gmail.com>
2021-05-13 02:25:01 +0300

    next

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import {
+  BookmarksIcon,
+  ExploreIcon,
+  HomeIcon,
+  ListsIcon,
+  MessagesIcon,
+  MoreIcon,
+  NotificationsIcon,
+  ProfileIcon,
+} from "../icons/Icon";
 
-const sidebar = () => {
+const Sidebar= () => {
     return (
-        <div className="w-72 bg-blue-200">
-        <h1>Sidebar</h1>
-        </div>
-    )
-}
+        <div className=" w-72 bg-blue-200">
+            <h1>Sidebar</h1>
+            <HomeIcon/>
+            <ExploreIcon />
+            <NotificationsIcon/>
+            <MessagesIcon/>
+            <BookmarksIcon/>
+            <ListsIcon/>
+            <ProfileIcon/>
+            <MoreIcon/>
+             </div> 
+  );
+};
 
-export default sidebar
+export default Sidebar;
+
