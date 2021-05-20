c32ce22103411e64a1cbf3d3c8fb72ac6d63f6f9
Gökalp Avcu <avcu219@gmail.com>
2021-05-20 16:56:40 +0300

    twitter-clone

---
+++ src/components/UserBox.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const UserBox = () => {
+  return (
+    <div className="flex justify-between items-center mb-6 hover:bg-primary-light cursor-pointer rounded-full py-2 px-4 transform transition-colors duration-200">
+      <img
+        src="https://twitter.com/GokalpAvcu/photo"
+        alt="Profile"
+        className="w-11 h-11 rounded-full"
+      />
+      <div className="flex flex-col">
+        <span className="font-bold text-md text-black">Gökalp Avcu</span>
+        <span className="text-sm text-gray-dark">@GokalpAvcu</span>
+      </div>
+
+      <div className="flex space-x-1">
+        <div className="w-1 h-1 bg-gray-900 rounded-full" />
+        <div className="w-1 h-1 bg-gray-900 rounded-full" />
+        <div className="w-1 h-1 bg-gray-900 rounded-full" />
+      </div>
+    </div>
+  );
+};
+
+export default UserBox;
--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import SideLink from "../components/SideLink";
+import UserBox from "../components/UserBox";
 import {
   BookmarksIcon,
   ExploreIcon,
@@ -13,70 +14,73 @@ import {
 import twitterLogo from "../images/twitter.svg";
 
 const sideLinks = [
-    {
-      name: "Home",
-      icon: HomeIcon,
-    },
-    {
-      name: "Explore",
-      icon: ExploreIcon,
-    },
-    {
-      name: "Notifications",
-      icon: NotificationsIcon,
-    },
-    {
-      name: "Messages",
-      icon: MessagesIcon,
-    },
-    {
-      name: "Bookmarks",
-      icon: BookmarksIcon,
-    },
-    {
-      name: "Lists",
-      icon: ListsIcon,
-    },
-    {
-      name: "Profile",
-      icon: ProfileIcon,
-    },
-    {
-      name: "More",
-      icon: MoreIcon,
-    },
-  ];
+  {
+    name: "Home",
+    icon: HomeIcon,
+  },
+  {
+    name: "Explore",
+    icon: ExploreIcon,
+  },
+  {
+    name: "Notifications",
+    icon: NotificationsIcon,
+  },
+  {
+    name: "Messages",
+    icon: MessagesIcon,
+  },
+  {
+    name: "Bookmarks",
+    icon: BookmarksIcon,
+  },
+  {
+    name: "Lists",
+    icon: ListsIcon,
+  },
+  {
+    name: "Profile",
+    icon: ProfileIcon,
+  },
+  {
+    name: "More",
+    icon: MoreIcon,
+  },
+];
 
-const Sidebar= () => {
-    return (
-        <div className=" flex flex-col justify-between w-72 px-2">
-            <div >
-                <div className=" mt-1 mb-4 ml-1 flex items-center justify-center w-12 h-12 rounded-full hover:bg-gray-lightest transform transition-colors duration-200">
-                <img src={twitterLogo} alt="Twitter Logo" className="w-9 h-9"></img>
-                </div>
-          
-           <nav className="mb-4">
-            <ul>
+const Sidebar = () => {
+  const [active, setActive] = useState("Home");
+
+  const handleMenuItemClick = (name) => {
+    setActive(name);
+  };
+
+  return (
+    <div className="h-screen sticky top-0 flex flex-col justify-between w-72 px-2">
+      <div>
+        <div className="mt-1 mb-4 ml-1 flex items-center justify-center w-12 h-12 rounded-full hover:bg-gray-lightest transform transition-colors duration-200">
+          <img src={twitterLogo} alt="Twitter Logo" className="w-9 h-9" />
+        </div>
+        <nav className="mb-4">
+          <ul>
             {sideLinks.map(({ name, icon }) => (
-                    <SideLink
-                    key={name}
-                    name={name}
-                    Icon={icon}
-                  />
-
-                ))}
-                
-                
-            </ul>
-            </nav>
-            <button className="bg-primary-base hover:bg-primary-dark text-white shadow-lg rounded-full py-3 px-8 w-11/12 transform transition-colors duration-200">Tweet</button>
-
-            </div>
-            <div>Alt taraf</div>
-           
-         </div> 
+              <SideLink
+                key={name}
+                name={name}
+                Icon={icon}
+                active={active}
+                onMenuItemClick={handleMenuItemClick}
+              />
+            ))}
+          </ul>
+        </nav>
+        <button className="bg-primary-base hover:bg-primary-dark text-white shadow-lg rounded-full py-3 px-8 w-11/12 transform transition-colors duration-200">
+          Tweet
+        </button>
+      </div>
+      <UserBox />
+    </div>
   );
 };
 
 export default Sidebar;
-
